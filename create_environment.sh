d643527ff2e73c35c4c803f83407508cd6c004e9
root <root@Melissa.>
2025-06-18 11:31:25 +0200

    yeewe

--- create_environment.sh
+++ create_environment.sh
@@ -2,3 +2,9 @@
 # We have to ask the user to input the his/her name (Will be needed to create the folder)                                             
 read -p "Enter your name: " yourName
 DIR="submission_reminder_$yourName"
+mkdir -p "$DIR"
+mkdir -p "$DIR/app"
+mkdir -p "$DIR/modules"
+mkdir -p "$DIR/assets"
+mkdir -p "$DIR/config"
+
