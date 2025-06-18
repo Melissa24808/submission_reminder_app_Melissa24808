323be19a6f1e27c5abba9e94e881e7f041a5f1a2
root <root@Melissa.>
2025-06-18 11:18:30 +0200

    step one

---
+++ create_environment.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# We have to ask the user to input the his/her name (Will be needed to create the folder)                                             
+read -p "Enter your name: " yourName
+DIR="submission_reminder_$yourName"
